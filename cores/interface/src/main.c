3e47aed46e2f0d99e2c4b1c9babb277f9637736f
skwort <skwort@skwort.dev>
2025-03-16 21:13:57 +1000

    cores: interface: Add heartbeat timer
    This commit implements the heartbeat timer in the interface core
    firmware.

--- cores/interface/src/main.c
+++ cores/interface/src/main.c
@@ -2,6 +2,7 @@
 #include <zephyr/logging/log.h>
 
 #include <app_version.h>
+#include <lib/heartbeat.h>
 
 LOG_MODULE_REGISTER(main);
 
@@ -9,6 +10,14 @@ int main(void)
 {
     printk("SENSE Interface Core version %s\n", APP_VERSION_STRING);
 
+#ifdef CONFIG_HEARTBEAT
+    if (heartbeat_init_start(K_MSEC(CONFIG_HEARTBEAT_DEFAULT_DURATION)))
+        LOG_ERR("Failed to initialise heartbeat.");
+#endif
+
+    while (1) {
+        k_sleep(K_MSEC(1000));
+    }
+
     return 0;
 }
- 
