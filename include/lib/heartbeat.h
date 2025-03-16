a21d712e6f325a4ca1b0971c1bb5bfd2de796548
skwort <skwort@skwort.dev>
2025-03-16 20:31:50 +1000

    lib: heartbeat: Implement heartbeat timer library
    This commit adds the implementation of a basic heartbeat timer.
    The timer is packaged in a library to allow it to be used across
    cores.
    
    The timer has a configurable duration and toggles the gpio aliased
    to heartbeat-gpio. This alias must appear in the devicetree, or the
    library will not compile.

--- include/lib/heartbeat.h
+++ include/lib/heartbeat.h
@@ -3,6 +3,32 @@
 
 #include <zephyr/kernel.h>
 
-int heartbeat_init(k_timeout_t duration);
+/**
+ * @defgroup lib_heartbeat Heartbeat timer library
+ * @ingroup lib
+ * @{
+ */
+
+/**
+ * @brief Initialises and starts the heartbeat timer.
+ *
+ * This function initialises the heartbeat GPIO and starts a timer with the
+ * specified duration. If the timer has already been initialized, it will
+ * simply restart the timer with a new duration. The heartbeat GPIO must be
+ * aliased in devicetree as "heartbeat-gpio".
+ *
+ * @param duration The timeout duration for the heartbeat timer.
+ * @return 0 on success, or a negative error code on failure.
+ */
+int heartbeat_init_start(k_timeout_t duration);
+
+/**
+ * @brief Stops the heartbeat timer.
+ *
+ * This function stops the heartbeat timer if it is running.
+ */
+void heartbeat_stop(void);
+
+/** @} */
 
 #endif /* LIB_HEARTBEAT_H_ */
--- lib/heartbeat/heartbeat.c
+++ lib/heartbeat/heartbeat.c
@@ -1,7 +1,63 @@
 #include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/logging/log.h>
+
 #include <lib/heartbeat.h>
 
-int heartbeat_init(k_timeout_t duration)
+LOG_MODULE_REGISTER(heartbeat, CONFIG_HEARTBEAT_LOG_LEVEL);
+
+#if !DT_NODE_EXISTS(DT_ALIAS(heartbeat_gpio))
+#error "No heartbeat gpio alias found in the device tree"
+#endif
+
+#define HEARTBEAT_NODE DT_ALIAS(heartbeat_gpio)
+
+static const struct gpio_dt_spec hb = GPIO_DT_SPEC_GET(HEARTBEAT_NODE, gpios);
+
+struct k_timer heartbeat_timer;
+static k_timeout_t timer_duration = K_MSEC(CONFIG_HEARTBEAT_DEFAULT_DURATION);
+static bool timer_initialised = false;
+
+extern void heartbeat_trigger(struct k_timer *timer_id)
 {
+    if (gpio_pin_toggle_dt(&hb)) {
+        LOG_ERR("Failed to toggle heartbeat");
+        return;
+    }
+
+    LOG_DBG("Heartbeat");
+    k_timer_start(&heartbeat_timer, timer_duration, timer_duration);
+}
+
+int heartbeat_init_start(k_timeout_t duration)
+{
+    int err;
+
+    timer_duration = duration;
+
+    if (timer_initialised)
+        goto start;
+
+    if (!gpio_is_ready_dt(&hb)) {
+        LOG_ERR("Heartbeat GPIO is not ready.");
+        return -ENXIO;
+    }
+
+    err = gpio_pin_configure_dt(&hb, GPIO_OUTPUT_ACTIVE);
+	if (err < 0) {
+		return err;
+	}
+
+    k_timer_init(&heartbeat_timer, &heartbeat_trigger, NULL);
+    timer_initialised = true;
+
+start:
+    k_timer_start(&heartbeat_timer, timer_duration, timer_duration);
     return 0;
 }
+
+void heartbeat_stop(void)
+{
+    k_timer_stop(&heartbeat_timer);
+}
+
