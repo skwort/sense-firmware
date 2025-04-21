23011988a6eb8baa99e65ed1b8e001dcb524c68d
skwort <skwort@skwort.dev>
2025-04-21 11:37:14 +1000

    lib: heartbeat: Disable heartbeat lib by default
    Disable the hearbeat library by default to improve the testability
    of libraries.
    
    This change makes the heartbeat library an opt-in feature, allowing
    for direct integration and unit testing of the library in a dedicated
    application.
    
    Furthermore, this uncouples libraries from each other, enabling
    independent development and testing of future additions.

--- include/lib/heartbeat.h
+++ include/lib/heartbeat.h
@@ -3,6 +3,8 @@
 
 #include <zephyr/kernel.h>
 
+#ifdef CONFIG_HEARTBEAT
+
 /**
  * @defgroup lib_heartbeat Heartbeat timer library
  * @ingroup lib
@@ -13,7 +15,7 @@
  * @brief Initialises and starts the heartbeat timer.
  *
  * This function initialises the heartbeat GPIO and starts a timer with the
- * specified duration. If the timer has already been initialized, it will
+ * specified duration. If the timer has already been initialised, it will
  * simply restart the timer with a new duration. The heartbeat GPIO must be
  * aliased in devicetree as "heartbeat-gpio".
  *
@@ -30,5 +32,7 @@ int heartbeat_init_start(k_timeout_t duration);
 void heartbeat_stop(void);
 
 /** @} */
+
+#endif /* CONFIG_HEARTBEAT */
 
 #endif /* LIB_HEARTBEAT_H_ */
